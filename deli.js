2b06bdc9dd2078abd83794cae63be092158c2ab5
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-05-13 20:09:10 +0300

    Form submission.

--- deli.js
+++ deli.js
@@ -19,3 +19,40 @@ backToTop.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 });
 
+/* Form submission */
+
+const form = document.querySelector("[data-form]");
+const formFields = document.querySelectorAll("[data-form-input]");
+const formSubmitBtn = document.querySelector("[data-form-btn]");
+
+for (let i = 0; i < formFields.length; i++) {
+    formFields[i].addEventListener("input", function () {
+        if (form.checkValidity()) {
+            formSubmitBtn.removeAttribute("disabled");
+        }
+    });
+}
+
+/* Emailjs implementation */
+
+emailjs.init("bfJIAK9oYio7zkqe2")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = {
+        from_name: form.fullname.value,
+        from_email: form.email.value,
+        title: form.topic.value,
+        message: form.message.value
+    }
+
+    emailjs.send("service_0juv8dt", "template_ukxfgdg", formData).then(function (response) {
+        alert("Message sent successfully!");
+        form.reset();
+        formSubmitBtn.setAttribute("disabled", "");
+    }, function (error) {
+        alert("Failed sending message. Please try again in a bit.")
+    });
+
+})
