7c578a3620deea4fd2978ef2f22ce9a668f3d2cb
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-05-15 17:06:41 +0300

    Back to home after message sent.

--- deli.js
+++ deli.js
@@ -1,3 +1,5 @@
+/* Back to top button */
+
 window.onscroll = function () {
     scrollFunction();
 };
@@ -18,6 +20,68 @@ backToTop.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
+
+/* Navigation */
+
+/* Sign home? */
+
+/* Products pages */
+
+/* let products = [{
+    "imageSRC": "./images/file1_f.jpg",
+    "title": "Филе Елена",
+    "price": "4.99лв",
+    "description": "Класическо мезе направено по рецепта от Елена.",
+    "ingredients": "Съставки: Свинско контрафиле, чубрица - ронена и смляна, сминдух, сол"
+},
+{
+    "imageSRC": "./images/file2_f.jpg",
+    "title": "Пастърма",
+    "price": "5.99лв",
+    "description": "Много вкусно пастърма с червен пипер и пушен червен пипер.",
+    "ingredients": "Съставки: Свинско контрафиле, червен пипер, пушен червен пипер, сол"
+}
+] */
+
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+const homePageLink = Array.from(navLinks).find(p => p.dataset = "landing");
+
+window.onload = function () {
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            const target = this.dataset.navLink;
+
+            const currentPage = Array.from(pages).find(p => !p.classList.contains("d-none"));
+            /* const currentLink = Array.from(navLinks).find(l => l.classList.contains("active")); */
+
+            if (currentPage && currentPage.dataset.page === target) return;
+
+            navLinks.forEach(link => link.classList.remove("active"));
+            this.classList.add("active");
+
+            // Add animationend handler once
+            const handleAnimationEnd = () => {
+                currentPage.classList.add("d-none");
+                currentPage.classList.remove("active");
+                currentPage.removeEventListener("animationend", handleAnimationEnd);
+
+                const newPage = Array.from(pages).find(p => p.dataset.page === target);
+                if (newPage) {
+                    newPage.classList.remove("d-none");
+                    scrollTo(0, 0);
+                }
+            };
+
+            currentPage.addEventListener("animationend", handleAnimationEnd);
+            currentPage.classList.add("active"); // Trigger animation
+        });
+    });
+}
+
+
+
 
 /* Form submission */
 
@@ -51,8 +115,10 @@ form.addEventListener("submit", function (e) {
         alert("Message sent successfully!");
         form.reset();
         formSubmitBtn.setAttribute("disabled", "");
+        homePageLink.click();
     }, function (error) {
         alert("Failed sending message. Please try again in a bit.")
     });
+
 
 })
